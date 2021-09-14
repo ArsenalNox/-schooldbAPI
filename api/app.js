0c82b0aa8f1ef57dea8fc854dae5ba56669f16ce
ArsenalNox <nohtimore@gmail.com>
2021-09-14 09:11:15 +0500

    базовые запросы

--- api/app.js
+++ api/app.js
@@ -1,8 +1,8 @@
 var express = require('express')
 var cluster = require('cluster')
-const { connect } = require('http2')
-const { SSL_OP_EPHEMERAL_RSA } = require('constants')
 var app = express()
+
+app.use(express.json())
 
 const port = 8080
 
@@ -14,6 +14,12 @@ if (cluster.isMaster){
         cluster.fork()
     }
 
+    cluster.on('exit', (worker)=>{
+        console.log(`Worker ${worker.id} died...\nCreating new worker...`)
+   
+        cluster.fork()
+    })    
+
 } else {
 
     function connect_to_database(){
@@ -23,11 +29,12 @@ if (cluster.isMaster){
             database: 'schools',
             host: 'localhost',
             user: 'root',
-            password: 'root'
+            password: ''
         })
 
         return con
     }
+
 
     app.get('/get_all_schools', (req, res) => {
         console.log(`cluster ${cluster.worker.id} responding to request`);
@@ -45,13 +52,11 @@ if (cluster.isMaster){
         })
     })
 
+
     app.get('/get_inactive_school', (req, res) => {
-        console.log(`cluster ${cluster.worker.id} responding to request`);
-        
         con = connect_to_database()
         con.connect((err)=>{
             if (err) throw err;
-            console.log(`Cluster ${cluster.worker.id} connected to database`)
             sql = 'SELECT * FROM `main` WHERE `main`.`isActive` = 0 LIMIT 1';
             con.query(sql, (err, result) => {
                 if (err) throw err;
@@ -60,6 +65,7 @@ if (cluster.isMaster){
             })
         })
     })
+
 
     app.get('/activate_school/:id', (req, res) =>{
         const id = req.params.id 
@@ -75,6 +81,80 @@ if (cluster.isMaster){
         })
     })
 
+    //Получает все муниципалитеты 
+    app.get('/get_all_mo', (req, res) => {
+        con = connect_to_database()
+        con.connect((err)=>{
+            if (err) throw err;
+            sql = "SELECT DISTINCT(mo) AS 'mo' FROM `main`"
+            con.query(sql, (err, result) => {
+                if (err) throw err;
+                res.send(result)
+            })
+        })
+    })
+
+
+    //Получает все школы данного муниципалитета 
+    app.get('/get_mo_schools', (req, res) => {
+        if (!req.body.mo){
+            res.status(400).send({err: "'mo' field is required"})
+        }
+
+        con = connect_to_database()
+        con.connect((err) => {
+            if (err) throw err;
+            mo = req.body.mo
+            sql = "SELECT id, name_full, name_short, type FROM `main` WHERE mo = '"+mo+"'"
+            con.query(sql, (err, result) =>{
+                if (err) throw err;
+                res.status(200).send(result)
+            })
+        })  
+    })
+
+    app.get('/:school_id/classes', (req, res) => {
+        const school_id = req.params.school_id
+        con = connect_to_database()
+
+        con.connect((err) => {
+            if (err) throw err;
+            sql = "SELECT * FROM `classes` WHERE `classes`.`sid` = '"+school_id+"'"
+
+            con.query(sql, (err, result) => {
+                if (err) throw err;
+                res.status(200).send(result)
+            })
+        })
+    })
+
+
+    app.get('/:school_id/:class_id/students', (req, res) => {
+        const school_id = req.params.school_id
+        const class_id  = req.params.class_id
+        con = connect_to_database()
+
+        con.connect((err) => {
+            if (err) throw err;
+            sql = "SELECT * FROM `students` WHERE `students`.`sid` = '"+school_id+"' AND `students`.`cid`='"+class_id+"' " 
+
+            con.query(sql, (err, result) => {
+                if (err) throw err;
+                res.status(200).send(result)
+            })
+        })
+    })
+
+
+    app.post('/:school_id/:class_id/:student_id/auth') 
+
+    //Последующие три требуют токена аунтефикации для действий 
+    app.post('/:school_id/:class_id/:student_id/logout')
+    app.post('/:school_id/:class_id/:student_id/get_avaliable_tests')
+	app.post('/:school_id/:class_id/:student_id/start_test/:test_id')
+    app.post('/:school_id/:class_id/:student_id/end_test/')
+    
+
     app.listen(port)
-    console.log(`New cluster (${cluster.worker.id}) fork app is listening on port ${port}`)
+    console.log(`New cluster fork (${cluster.worker.id}) is listening on port ${port}`)
 }
